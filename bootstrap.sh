b5818db623135c8abca307994d82f5546d015cc6
Grigori Kartashyan <grigori.kartashyan@gmail.com>
2017-10-08 17:28:07 +0400

    Create separate conf file for apache.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,12 @@ DOMAIN=.me
 EMAIL_ADDR=grigori.kartashyan@gmail.com
 ERROR_LOG_FILE=error.log
 CUSTOM_LOG_FILE=custom.log
-APACHE_DEFAULT_CONF=/etc/apache2/sites-available/000-default.conf
-APACHE_CONF=/etc/apache2/apache2.conf
+
+APACHE_PATH=/etc/apache2
+APACHE_DEFAULT_CONF=${APACHE_PATH}/sites-available/000-default.conf
+PRJ_APACHE_CONF=${APACHE_PATH}/sites-available/${PRJ_NAME}${DOMAIN}.conf
+APACHE_CONF=${APACHE_PATH}/apache2.conf
+
 IP_ADDRESS=`ifconfig |grep -A 2 eth1 |grep "inet addr" |awk '{print $2}'|awk -F ":" '{print $2}'`
 LOG_PATH=/var/www/logs
 PRJ_PATH=/var/www
@@ -18,7 +22,7 @@ GROUP_NAME='vagrant'
 apt-get update
 apt-get -y install git
 apt-get -y install python-pip
-pip install Django==1.11
+pip install Django==1.11.6
 apt-get -y install apache2
 apt-get -y install python-setuptools
 apt-get -y install libapache2-mod-wsgi
@@ -28,9 +32,9 @@ cd ${REPO_PATH}
 git init --bare
 
 echo "#!/bin/sh" | tee -a ${REPO_PATH}/hooks/post-receive
-#echo "rm -rf ${LIVE_PATH}/*" | tee -a ${REPO_PATH}/hooks/post-receive
 echo "git --work-tree=${LIVE_PATH} --git-dir=${REPO_PATH} checkout -f" | tee -a ${REPO_PATH}/hooks/post-receive
-echo "find ${PRJ_PATH}/${PRJ_NAME} -name settings.py -exec sed -i \"s/ALLOWED_HOSTS\\ \\=\\ \\[\\]/ALLOWED_HOSTS\\ \\=\\ \\[\\'${IP_ADDRESS}\\'\\,\ \\'localhost\\'\\,\ \\'127\\.0\\.0\\.1\\'\\]/g\" {} \\;" | tee -a ${REPO_PATH}/hooks/post-receive
+#sed -i '/TEXT_TO_BE_REPLACED/c\This line is removed by the admin.' /tmp/foo
+echo "find ${PRJ_PATH}/${PRJ_NAME} -name settings.py -exec sed -i \"s/ALLOWED_HOSTS\\ \\=\\ \\[\\]/ALLOWED_HOSTS\\ \\=\\ \\[\\'${IP_ADDRESS}\\'\\,\ \\'localhost\\'\\,u\\'${PRJ_NAME}${DOMAIN}\\'\\,\ \\'127\\.0\\.0\\.1\\'\\]/g\" {} \\;" | tee -a ${REPO_PATH}/hooks/post-receive
 chmod +x ${REPO_PATH}/hooks/post-receive
 
 chgrp -R ${GROUP_NAME} ${REPO_PATH}
@@ -42,24 +46,25 @@ chmod 777 ${LOG_PATH}
 
 mkdir -p ${PRJ_PATH}/${PRJ_NAME}
 chmod 777 ${PRJ_PATH}/${PRJ_NAME}
-#django-admin.py startproject ${PRJ_NAME}
-#find ${PRJ_PATH}/${PRJ_NAME} -name settings.py -exec sed -i "s/ALLOWED_HOSTS\ \=\ \[\]/ALLOWED_HOSTS\ \=\ \[\'${IP_ADDRESS}\'\, \'localhost\'\, \'127\.0\.0\.1\'\]/g" {} \;
-#cd -
 
-echo "<VirtualHost *:80>" | tee ${APACHE_DEFAULT_CONF}
-echo "        ServerName ${PRJ_NAME}${DOMAIN}" | tee -a ${APACHE_DEFAULT_CONF}
-echo "        ServerAlias www.${PRJ_NAME}${DOMAIN}" | tee -a ${APACHE_DEFAULT_CONF}
-echo "        ServerAdmin ${EMAIL_ADDR}" | tee -a ${APACHE_DEFAULT_CONF}
-echo "        DocumentRoot ${PRJ_PATH}/${PRJ_NAME}" | tee -a ${APACHE_DEFAULT_CONF}
-echo "        WSGIScriptAlias / ${PRJ_PATH}/${PRJ_NAME}/${PRJ_NAME}/wsgi.py" | tee -a ${APACHE_DEFAULT_CONF}
-echo "        ErrorLog ${LOG_PATH}/${ERROR_LOG_FILE}" | tee -a ${APACHE_DEFAULT_CONF}
-echo "        CustomLog ${LOG_PATH}/${CUSTOM_LOG_FILE} combined" | tee -a ${APACHE_DEFAULT_CONF}
-echo "</VirtualHost>" | tee -a ${APACHE_DEFAULT_CONF}
+echo "<VirtualHost *:80>" | tee ${PRJ_APACHE_CONF}
+echo "        ServerName ${PRJ_NAME}${DOMAIN}" | tee -a ${PRJ_APACHE_CONF}
+echo "        ServerAlias www.${PRJ_NAME}${DOMAIN}" | tee -a ${PRJ_APACHE_CONF}
+echo "        ServerAdmin ${EMAIL_ADDR}" | tee -a ${PRJ_APACHE_CONF}
+echo "        DocumentRoot ${PRJ_PATH}/${PRJ_NAME}" | tee -a ${PRJ_APACHE_CONF}
+echo "        WSGIScriptAlias / ${PRJ_PATH}/${PRJ_NAME}/${PRJ_NAME}/wsgi.py" | tee -a ${PRJ_APACHE_CONF}
+echo "        ErrorLog ${LOG_PATH}/${ERROR_LOG_FILE}" | tee -a ${PRJ_APACHE_CONF}
+echo "        CustomLog ${LOG_PATH}/${CUSTOM_LOG_FILE} combined" | tee -a ${PRJ_APACHE_CONF}
+echo "</VirtualHost>" | tee -a ${PRJ_APACHE_CONF}
+
+#Enable site
+ln -s ${PRJ_APACHE_CONF} ${APACHE_PATH}/sites-enabled/${PRJ_NAME}${DOMAIN}.conf
 
 echo "WSGIPythonPath ${PRJ_PATH}/${PRJ_NAME}" | tee -a ${APACHE_CONF}
 
 service apache2 restart
 echo "Server address is ${IP_ADDRESS}"
-echo "In your project add this remote: 'git remote add live ssh://vagrant@${IP_ADDRESS}/${REPO_PATH}' "
+echo "In your project add this remote: 'git remote add live ssh://vagrant@${IP_ADDRESS}${REPO_PATH}' "
 echo "Use password 'vagrant' "
-
+echo "Also you should add '${IP_ADDRESS}	${PRJ_NAME}${DOMAIN}' mapping in /etc/hosts file"
+echo "Now you can reach the site by typing in browser ${PRJ_NAME}${DOMAIN}"
