b8e96c17e313df38fdea2aeca5441b0e60085395
Grigori Kartashyan <grigori.kartashyan@gmail.com>
2017-10-08 13:08:06 +0400

    Add deployment scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash +x
 
-PRJ_NAME=grigori
+PRJ_NAME=website
 DOMAIN=.me
 EMAIL_ADDR=grigori.kartashyan@gmail.com
 ERROR_LOG_FILE=error.log
@@ -10,20 +10,41 @@ APACHE_CONF=/etc/apache2/apache2.conf
 IP_ADDRESS=`ifconfig |grep -A 2 eth1 |grep "inet addr" |awk '{print $2}'|awk -F ":" '{print $2}'`
 LOG_PATH=/var/www/logs
 PRJ_PATH=/var/www
+REPO_NAME=${PRJ_NAME}${DOMAIN}.git
+REPO_PATH=/repos/${REPO_NAME}
+LIVE_PATH=${PRJ_PATH}/${PRJ_NAME}
+GROUP_NAME='vagrant'
 
 apt-get update
+apt-get -y install git
 apt-get -y install python-pip
 pip install Django==1.11
 apt-get -y install apache2
 apt-get -y install python-setuptools
 apt-get -y install libapache2-mod-wsgi
 
+mkdir -p ${REPO_PATH}
+cd ${REPO_PATH}
+git init --bare
+
+echo "#!/bin/sh" | tee -a ${REPO_PATH}/hooks/post-receive
+#echo "rm -rf ${LIVE_PATH}/*" | tee -a ${REPO_PATH}/hooks/post-receive
+echo "git --work-tree=${LIVE_PATH} --git-dir=${REPO_PATH} checkout -f" | tee -a ${REPO_PATH}/hooks/post-receive
+echo "find ${PRJ_PATH}/${PRJ_NAME} -name settings.py -exec sed -i \"s/ALLOWED_HOSTS\\ \\=\\ \\[\\]/ALLOWED_HOSTS\\ \\=\\ \\[\\'${IP_ADDRESS}\\'\\,\ \\'localhost\\'\\,\ \\'127\\.0\\.0\\.1\\'\\]/g\" {} \\;" | tee -a ${REPO_PATH}/hooks/post-receive
+chmod +x ${REPO_PATH}/hooks/post-receive
+
+chgrp -R ${GROUP_NAME} ${REPO_PATH}
+chmod -R g+rwX ${REPO_PATH}
+find ${REPO_PATH} -type d -exec chmod g+s '{}' +
+
 mkdir -p ${LOG_PATH}
 chmod 777 ${LOG_PATH}
 
-cd ${PRJ_PATH}
-django-admin.py startproject ${PRJ_NAME}
-find ${PRJ_PATH}/${PRJ_NAME} -name settings.py -exec sed -i "s/ALLOWED_HOSTS\ \=\ \[\]/ALLOWED_HOSTS\ \=\ \[\'${IP_ADDRESS}\'\, \'localhost\'\, \'127\.0\.0\.1\'\]/g" {} \;
+mkdir -p ${PRJ_PATH}/${PRJ_NAME}
+chmod 777 ${PRJ_PATH}/${PRJ_NAME}
+#django-admin.py startproject ${PRJ_NAME}
+#find ${PRJ_PATH}/${PRJ_NAME} -name settings.py -exec sed -i "s/ALLOWED_HOSTS\ \=\ \[\]/ALLOWED_HOSTS\ \=\ \[\'${IP_ADDRESS}\'\, \'localhost\'\, \'127\.0\.0\.1\'\]/g" {} \;
+#cd -
 
 echo "<VirtualHost *:80>" | tee ${APACHE_DEFAULT_CONF}
 echo "        ServerName ${PRJ_NAME}${DOMAIN}" | tee -a ${APACHE_DEFAULT_CONF}
@@ -39,4 +60,6 @@ echo "WSGIPythonPath ${PRJ_PATH}/${PRJ_NAME}" | tee -a ${APACHE_CONF}
 
 service apache2 restart
 echo "Server address is ${IP_ADDRESS}"
+echo "In your project add this remote: 'git remote add live ssh://vagrant@${IP_ADDRESS}/${REPO_PATH}' "
+echo "Use password 'vagrant' "
 
